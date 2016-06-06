d3fa773f009bdec22f879413e76af7c3e4e95516
aaronhsumath <aaronsemail@gmail.com>
2016-06-06 16:20:13 -0700

    Added plots for price per square foot and median days on market.
    Heights 10, 6, 8, 6. Same height of 900.
    Script also loads kangaroo2 (second set of data containing PSF and DOM data).

--- server.R
+++ server.R
@@ -9,6 +9,7 @@ library(scales)
 
 # Load data
 load("kangaroo.RData")
+load("kangaroo2.RData")
 
 # Shiny server script
 shinyServer(function(input, output) {
@@ -61,19 +62,65 @@ shinyServer(function(input, output) {
 
     # xlim(max(subset(kangaroo, kangaroo[, "type"] == input$selectType & kangaroo[, "city"] # Developer: Aaron Hsu (aaronhsu.re[at]removethis.gmail.com) # %in% input$selectCity)), min(subset(kangaroo, kangaroo[, "type"] == input$selectType & kangaroo[, "city"] %in% input$selectCity)))
     
+    # Generate plot for psf
+    p3 <- ggplot(
+      data = subset(kangaroo2, 
+                    kangaroo2[, "type"] == input$selectType & kangaroo2[, "city"] %in% input$selectCity),
+      aes(
+        x = year,
+        y = psf,
+        group = city,
+        color = city
+      )
+    ) +
+      geom_point() +
+      geom_line() +
+      ggtitle("Price per Square Foot") +
+      labs(y = "Price per Square Foot",
+           x = "Year") +
+      scale_colour_discrete(name="") +
+      theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.3)) 
+
+    
+    
+    # Generate plot for dom
+    p4 <- ggplot(
+      data = subset(kangaroo2, 
+                    kangaroo2[, "type"] == input$selectType & kangaroo2[, "city"] %in% input$selectCity),
+      aes(
+        x = year,
+        y = dom,
+        group = city,
+        color = city
+      )
+    ) +
+      geom_point() +
+      geom_line() +
+      ggtitle("Median Days on Market") +
+      labs(y = "Median Days on Market",
+           x = "Year") +
+      scale_colour_discrete(name="") +
+      theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.3)) 
+    
+    
+    
     # Convert plots into gtables
     p1 <- ggplot_gtable(ggplot_build(p1))
     p2 <- ggplot_gtable(ggplot_build(p2))
+    p3 <- ggplot_gtable(ggplot_build(p3))
+    p4 <- ggplot_gtable(ggplot_build(p4))
     
     # Calculate max widths of the plots
-    maxWidth = unit.pmax(p1$widths[2:3], p2$widths[2:3])
+    maxWidth = unit.pmax(p1$widths[2:3], p2$widths[2:3], p3$widths[2:3], p4$widths[2:3])
     
     # Set the widths to be the same
     p1$widths[2:3] <- maxWidth
     p2$widths[2:3] <- maxWidth
+    p3$widths[2:3] <- maxWidth
+    p4$widths[2:3] <- maxWidth
     
     # Draw the plots
-    grid.arrange(p1, p2, heights = c(10,6))
+    grid.arrange(p1, p2, p3, p4, heights = c(10, 6, 8, 6))
     
   }, height = 900)
   
